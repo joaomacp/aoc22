daa499c2900f16770db0536e68f299ae3febb4ab
joaomacp <95joaopereira@gmail.com>
2022-12-21 11:36:46 +0000

    day16 part 1

--- day16/day16.py
+++ day16/day16.py
@@ -7,21 +7,14 @@ with open("input/day16.txt") as f:
     volcano[tokens[0]] = [int(tokens[1]), tokens[2:]]
 
 memo = dict()
-memo_exact = dict()
 
 def optimal_pressure(current_valve, time_left, open_valves, pressure):
   if time_left == 1:
     return pressure
-  if (current_valve, time_left, tuple(open_valves)) in memo_exact:
-    return memo_exact[(current_valve, time_left, tuple(open_valves))] + pressure
-  if (current_valve, time_left) in memo:
-    closed_valves = [valve for valve in volcano if valve not in open_valves]
-    potential = sum([volcano[valve][0] * (time_left-1) for valve in closed_valves])
-    if potential < memo[(current_valve, time_left)]:
-      return 0
-    # return memo[(current_valve, time_left)] + pressure
+  if (current_valve, time_left, tuple(open_valves)) in memo:
+    return memo[(current_valve, time_left, tuple(open_valves))] + pressure
   options = []
-  if current_valve not in open_valves: # Valve is closed, opening is an option
+  if current_valve not in open_valves and volcano[current_valve][0] > 0: # Valve is closed and positive, opening is an option
     valve_pressure = volcano[current_valve][0]
     options.append(optimal_pressure(current_valve, time_left-1, sorted(open_valves + [current_valve]), \
                                     pressure + valve_pressure * (time_left-1)))
@@ -29,8 +22,7 @@ def optimal_pressure(current_valve, time_left, open_valves, pressure):
     options.append(optimal_pressure(neighbour, time_left-1, sorted(open_valves), \
                                     pressure))
   val = max(options)
-  memo[(current_valve, time_left)] = val - pressure
-  memo_exact[(current_valve, time_left, tuple(open_valves))] = val - pressure
+  memo[(current_valve, time_left, tuple(open_valves))] = val - pressure
   return val
 
 print("Part 1:", optimal_pressure("AA", 30, [], 0))
